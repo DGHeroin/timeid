afdb601d98d73db2228865a2edeba1b1b473ee0d
DGHeroin <DGHeroin@gmail.com>
2022-04-21 14:29:26 +0800

    update

--- time_id.go
+++ time_id.go
@@ -6,37 +6,32 @@ import (
 )
 
 var (
-    customEpoch int64 = 1577836800000 // 1274257488896 in 2010-05-19 08:24:48
-    stepBits    uint8 = 22
+    EpochMS  int64 = 1640995200000 // 2022-01-01 00:00:00.000 - max date 2056-11-04 03:53:47.775
+    StepBits uint8 = 20 // 1048575 step in a millisecond
+    StepMask int64 = -1 ^ (-1 << StepBits)
 )
 
 type (
     Node struct {
-        mu        sync.Mutex
-        time      int64
-        epoch     time.Time
-        step      int64
-        timeShift uint8
-        stepMask  int64
+        mu    sync.Mutex
+        time  int64
+        epoch time.Time
+        step  int64
     }
 )
 
 func NewNode() *Node {
     n := &Node{}
-    n.timeShift = stepBits
-    n.stepMask = -1 ^ (-1 << stepBits)
-
-    var curTime = time.Now()
-    // add time.Duration to curTime to make sure we use the monotonic clock if available
-    n.epoch = curTime.Add(time.Unix(customEpoch/1000, (customEpoch%1000)*1000000).Sub(curTime))
+    var curTime = GetTime()
+    n.epoch = curTime.Add(time.Unix(EpochMS/1000, (EpochMS%1000)*1000000).Sub(curTime))
     return n
 }
 func (n *Node) Generate() int64 {
     n.mu.Lock()
     defer n.mu.Unlock()
-    now := time.Since(n.epoch).Nanoseconds() / 1000000
+    var now = time.Since(n.epoch).Nanoseconds() / 1000000
     if now == n.time {
-        n.step = (n.step + 1) & n.stepMask
+        n.step = (n.step + 1) & StepMask
 
         if n.step == 0 {
             for now <= n.time {
@@ -48,16 +43,55 @@ func (n *Node) Generate() int64 {
     }
     n.time = now
 
-    r := (now)<<n.timeShift |
+    r := (now)<<StepBits |
         (n.step)
 
     return r
 }
 
-func (n *Node) Reverse(id int64) (time.Time, int64) {
-    ms := id>>stepBits + customEpoch
+func IdReverse(id int64) (time.Time, int64) {
+    ms := id>>StepBits + EpochMS
     t := time.Unix(0, int64(time.Millisecond)*ms)
-
-    step := id & n.stepMask
+    step := id & StepMask
     return t, step
+}
+func IdReverseMs(id int64) int64 {
+    t, _ := IdReverse(id)
+    return t.UnixNano() / 1000000
+}
+
+func TimeMsToId(ms int64, step int64) int64 {
+    return (ms-EpochMS)<<StepBits | (step)
+}
+func TimeParseMs(layout, value string) (int64, error) {
+    t, err := time.Parse(layout, value)
+    if err != nil {
+        return 0, err
+    }
+    return t.UnixNano() / int64(time.Millisecond), nil
+}
+func TimeMsToTime(ms int64) time.Time {
+    return time.Unix(0, int64(time.Millisecond)*ms).UTC()
+}
+func timelyMs(layout string, ms int64) int64 {
+    value, _ := time.Parse(layout, TimeMsToTime(ms).Format(layout))
+    return value.UTC().UnixNano() / 1000000
+}
+func TimeMsToMinutely(ms int64) int64 {
+    return timelyMs("2006-01-02 15:04", ms)
+}
+func TimeMsToHourly(ms int64) int64 {
+    return timelyMs("2006-01-02 15", ms)
+}
+func TimeMsToDaily(ms int64) int64 {
+    return timelyMs("2006-01-02", ms)
+}
+func TimeMsToMonthly(ms int64) int64 {
+    return timelyMs("2006-01", ms)
+}
+func TimeMsToYearly(ms int64) int64 {
+    return timelyMs("2006", ms)
+}
+func GetTime() time.Time {
+    return time.Now().UTC()
 }
--- time_id_test.go
+++ time_id_test.go
@@ -6,10 +6,10 @@ import (
 )
 
 func TestNewNode(t *testing.T) {
-    ts, _ := time.Parse("2006-01-02 15:04:05", "2010-05-19 08:24:48.8964")
+    ts, _ := time.Parse("2006-01-02 15:04:05", "2022-01-01 00:00:00.000")
     t.Log(ts, ts.UnixMilli())
-    t.Log("max step in one millisecond:",-1^(-1<<(22)))
-    t.Log("max date support:", time.Unix(0, (-1^(-1<<(64-22)) + ts.UnixMilli())*int64(time.Millisecond)))
+    t.Log("max step in one millisecond:",-1^(-1<<(StepBits)))
+    t.Log("max date support:", time.Unix(0, (-1^(-1<<(64-StepBits)) + ts.UnixMilli())*int64(time.Millisecond)))
 }
 
 func TestNode_Generate(t *testing.T) {
@@ -29,9 +29,9 @@ func TestNode_Reverse(t *testing.T) {
     b := node.Generate()
     c := node.Generate()
 
-    at, as := node.Reverse(a)
-    bt, bs := node.Reverse(b)
-    ct, cs := node.Reverse(c)
+    at, as := IdReverse(a)
+    bt, bs := IdReverse(b)
+    ct, cs := IdReverse(c)
 
     t.Log(a, at, as)
     t.Log(b, bt, bs)
